2abf3f2ce7cd26aecba1f15af721b6621f8487f5
chidambara raja <chidam.sky@gmail.com>
2019-05-23 23:13:15 +0530

    valKeyFunction

---
+++ com/functions/FunctionUsingValKey.scala
@@ -0,0 +1,14 @@
+package com.functions
+
+object FunctionUsingValKey {
+  
+//  A function using val keyword
+  val function = (cost : Int) =>{
+    val disc=10
+    cost - disc
+  }
+  def main(args: Array[String]): Unit = {
+    print(function(9))
+    println(10)
+  }
+}
